4448eb293a60883d818ee47b4d6fbf72e69ac722
Serhiy Savchuk <serhiy@Serhiys-iMac.home>
2019-06-04 23:35:33 -0700

    Modify Bootstrap elements.

---
+++ flashcards-master/js/scripts.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $(".initially-showing1").toggle();
+    $(".initially-hidden1").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $(".initially-showing2").toggle();
+    $(".initially-hidden2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable3").click(function() {
+    $(".initially-showing3").toggle();
+    $(".initially-hidden3").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable4").click(function() {
+    $(".initially-showing4").toggle();
+    $(".initially-hidden4").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable5").click(function() {
+    $(".initially-showing5").toggle();
+    $(".initially-hidden5").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable6").click(function() {
+    $(".initially-showing6").toggle();
+    $(".initially-hidden6").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable7").click(function() {
+    $(".initially-showing7").toggle();
+    $(".initially-hidden7").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable8").click(function() {
+    $(".initially-showing8").toggle();
+    $(".initially-hidden8").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable9").click(function() {
+    $(".initially-showing9").toggle();
+    $(".initially-hidden9").toggle();
+  });
+});
+
+
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//     $("p").click(function(){
+//     $(".initially-showing1").show();
+//     $(".initially-hidden1").hide();
+//   });
+// });
+
+// fadeIn()
+// fadeOut()
+// fadeToggle()
+// slideDown()
+// slideUp()
+// slideToggle()
